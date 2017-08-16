40c411cc3693328221fcc6c05cc2e831df026fbf
Imani Oliver <isaac@ihardy.co>
2017-08-16 19:31:00 -0400

    all functionality

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 
 let tasks = [];
-
+let complete = [];
 
 
 router.get("/", function(req, res) {
-    res.render("layout", {tasks: tasks})
+    res.render("layout", {tasks: tasks , complete: complete})
 });
 
 
@@ -20,6 +20,22 @@ router.post("/", function(req, res) {
 
 });
 
+
+router.post("/a", function(req, res){
+
+    // if (newTask) {
+    //
+    // }
+
+
+    tasks.splice(tasks.indexOf(req.body.theTask),1);
+
+    complete.push(req.body.theTask) //make sure that this is putting into the complete object
+    console.log(complete); //tell it where to to go
+
+    res.redirect('/');
+
+})
 
 
 
